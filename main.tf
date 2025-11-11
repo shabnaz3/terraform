19dab33e37263e05cddf6be81cae53a715191eca
skhanam <97446340+shabnaz3@users.noreply.github.com>
2025-11-11 10:57:11 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,16 @@
+provider "kubernetes" {
+  # Configuration options
+    config_path = "/etc/rancher/k3s/k3s.yaml"
+
+}
+provider "helm" {
+  kubernetes {
+    config_path = "/etc/rancher/k3s/k3s.yaml"
+  }
+}
+
+provider "gitlab" {
+  token = "glpat-zvrFwYExgXue1Fz9Rmm"  #gitlab token
+  base_url = "https://gitlab.com/"
+}
 
